a13046e7ffad3a2c4b91776ed4b4303a8eafac15
tallroids <har05054@byui.edu>
2017-06-20 13:52:05 -0600

    updated generator

--- generate.js
+++ generate.js
@@ -3,17 +3,47 @@
 
 var fs = require('fs'),
   structure = require('./structure.js'),
+  stories = require('./stories.js'),
   d3 = require('d3-dsv'),
   htmlFiles = require('./htmlFilesToObject.js')(),
   handlebars = require('Handlebars'),
   lessonPage, practicesPage, page = 0,
-  template, location;
+  template, template2, location;
+
+function zeroed(lessonNumber) {
+  if (lessonNumber < 10) {
+    return "0" + lessonNumber;
+  } else {
+    return lessonNumber;
+  }
+}
 
 if (typeof (process.argv[2]) == 'undefined') {
   location = 'docs'
 } else {
   location = 'devPages'
 }
+
+var quizzes = d3.csvParse(fs.readFileSync('allTheQuizesFilteredCols.csv', 'utf8'));
+handlebars.registerHelper('generateLinks', function (lessonNumber, practiceLink) {
+  var html = "",
+    filtered;
+  if (practiceLink != "ST") {
+    /* class practice, and extra mile */
+    filtered = quizzes.filter(function (each) {
+      return each.title.slice(0, 9) == 'ECN_' + zeroed(lessonNumber) + '_' + practiceLink;
+    });
+  } else {
+    /* Stories */
+    filtered = quizzes.filter(function (each) {
+      return each.title.slice(0, 9) == 'ECN_' + zeroed(lessonNumber) + '_ST'
+    })
+  }
+  filtered.forEach(function (quiz) {
+    html += "<li><a href=" + quiz.html_url + " target='quizFrame'></a></li>";
+  });
+  return new handlebars.SafeString(html);
+});
 
 function small(string) {
   if (string == null || string == 'undefined') {
@@ -22,31 +52,6 @@ function small(string) {
     return string.replace(/ /g, "").toLowerCase().replace('?', "").replace("'", "").replace('!', '')
   }
 }
-
-function noWhite(string) {
-  return string.replace(/ /g, "");
-}
-
-handlebars.registerHelper('generateLinks', function (practice, lesson, topic, practice) {
-  var count = practicesPerPage[topic][practice][lesson];
-  var out = "";
-  var url = "";
-
-  function getUrl(i) {
-    quizes.forEach(function (quiz) {
-      if (quiz.title == practice + " " + lesson + " - " + topic + " - " + practice + " " + i) {
-        url = quiz.mobile_url;
-      }
-    });
-    return url;
-  }
-
-  for (var i = 0; i < count; i++) {
-    out += '<li id="' + small(lesson) + '_' + small(practice) + '_' + small(topic) + '_psg1" class="reset ' + small(practice) + ' passage"><a href="' + new handlebars.SafeString(getUrl(i + 1)) + '" target="_blank"></a> </li>\n';
-  }
-
-  return new handlebars.SafeString(out);
-});
 
 handlebars.registerHelper('generateTab1', function () {
   var html = "";
@@ -60,8 +65,7 @@ handlebars.registerHelper('generateTab1', function () {
 
 handlebars.registerHelper('generateTab2', function () {
   var html = "";
-  var lessonNumber = 24;
-  console.log(structure.lessons.slice(24))
+  var lessonNumber = 0;
   structure.lessons.slice(24).forEach(function (lesson) {
     lessonNumber += 1;
     html += '<a href="' + small(lesson) + '.html" class="tile"><p class="lessonTitle">Lesson ' + lessonNumber + '</p><p class="lessonSub">' + lesson + '</p></a>'
@@ -69,22 +73,31 @@ handlebars.registerHelper('generateTab2', function () {
   return new handlebars.SafeString(html);
 })
 
-
+function link(practice) {
+  if (practice == "Class Practice") {
+    return "CP"
+  } else if (practice == "Extra Mile") {
+    return "EM"
+  } else {
+    return "ST"
+  }
+}
 
 function generatePage(lesson, practice) {
-  var speakUp = 1;
+  var level = 1;
   var lessonNumber = structure.lessons.indexOf(lesson) + 1;
   if (lessonNumber > 24) {
-    speakUp = 2;
+    level = 2;
   }
 
   var context = {
     "lesson": lesson,
     "lessonNumber": lessonNumber,
-    "speakUp": speakUp,
+    "level": level,
     "practice": practice,
     "lessonsmall": small(lesson),
-    "practicesmall": small(practice)
+    "practicesmall": small(practice),
+    "practiceLink": link(practice)
   }
 
   if (practice == null) {
@@ -102,15 +115,44 @@ function generatePage(lesson, practice) {
   }
 }
 
-function generateHomePage() {
+function generateHomePages() {
   var context = {
     "lessons": structure.lessons
   }
-  template = handlebars.compile(htmlFiles.index);
-  fs.writeFileSync(location + '/index.html', template(context));
+  /* Page 1 */
+  template = handlebars.compile(htmlFiles.home1);
+  fs.writeFileSync(location + '/home1.html', template(context));
+  /* Page 2 */
+  template2 = handlebars.compile(htmlFiles.home2);
+  fs.writeFileSync(location + '/home2.html', template2(context));
 }
 
-generateHomePage();
+function generateStoryPages(lesson) {
+  /*var context = {
+    "stories": structure.lessons[lesson].stories
+  }
+  structure.lessons[lesson].forEach(function (each) {
+    template = handlebars.compile(htmlFiles.story)
+    fs.writeFileSync(location + '/' + lesson + '_' + each, template(context))
+  })*/
+  var level = 1;
+  var lessonNumber = structure.lessons.indexOf(lesson) + 1;
+  if (lessonNumber > 24) {
+    level = 2;
+  }
+  var context = {
+    lessonNumber: lessonNumber,
+    lesson: lesson,
+    lessonsmall: small(lesson),
+    level: level,
+    story: stories[lessonNumber],
+    practiceLink: "ST"
+  }
+  template = handlebars.compile(htmlFiles.story);
+  fs.writeFileSync(location + '/story_' + zeroed(lessonNumber) + '.html', template(context));
+}
+
+generateHomePages();
 structure.lessons.forEach(function (lesson) {
   //Generate the lesson page
   lessonPage = generatePage(lesson);
@@ -125,5 +167,10 @@ structure.lessons.forEach(function (lesson) {
     fs.writeFileSync(location + '/' + small(lesson) + "_" + small(practice) + '.html', practicesPage);
     page++;
     console.log("Wrote page " + page);
+    if (practice == "Stories") {
+      generateStoryPages(lesson)
+      page++;
+      console.log("Wrote page " + page);
+    }
   })
 })
